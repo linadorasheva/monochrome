403da8694bad146f3b3b29d1a75b94d10946fa8b
lina dorasheva <dorasheva.a@gmail.com>
2018-09-16 21:33:33 +0300

    написан скрипт реверса темы

---
+++ js/script_reverse.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var body = document.querySelector('body');
+var navLink = body.querySelector('.main-nav');
+var logo = body.querySelector('.logo');
+var descriptBtn = body.querySelector('.descript__btn');
+var copyright = body.querySelector('.copyright');
+var elementsReverse = [body, navLink, logo, descriptBtn, copyright];
+
+var reverseColor = function (evt) {
+  evt.preventDefault();
+  for (var i = 0; i < elementsReverse.length; i++) {
+    elementsReverse[i].classList.toggle('reverse');
+  }
+};
+
+descriptBtn.addEventListener('click', function (evt) {
+  reverseColor(evt);
+})
